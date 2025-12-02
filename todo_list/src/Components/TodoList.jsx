c4bdc545a74e508f8fd5e87b5480fe057aab7bcf
Fadhel <fadhel@tutamail.com>
2025-12-02 16:38:17 +0300

    update

--- todo_list/src/Components/TodoList.jsx
+++ todo_list/src/Components/TodoList.jsx
@@ -8,15 +8,40 @@ const TodoList = () => {
   const [listInputs, setListInputes] = useState({});
 
   const handleAddToDo = () => {
-    if (headingInput.trim() != '') {
-      setTodos([...todos, { heading: headingInput, lists: [] }]);
-      setHeadingInput("");
-    }
+    if (!headingInput.trim()) return;
+
+    const newTodo = {
+      id: crypto.randomUUID(),
+      heading: headingInput,
+      list: []
+    };
+
+    setTodos(prev => [...prev, newTodo]);
+    setHeadingInput("");
   }
 
-const handleDeleteTodo=(index)=>{
+  const handleDeleteTodo = (id) => {
+    setTodos(prev => prev.filter(todo => todo.id != id))
 
-}
+    /*const newTodos = [...todos]; newTodos.splice(index, 1); setTodos(newTodos); this works but not recommended it can mutate the array */
+  }
+
+  const handleAddList = (id) => {
+    const text = listInputs[id]?.trim();
+    if (!text) return;
+
+    setTodos(prev => prev.map(todos => todos.id === id
+      ? { ...todos, list: [...todos.list, text] }
+      : todos
+    ))
+
+    /*if (listInputs.trim() !== "") {
+      const newTodos = [...todos];
+      newTodos[index].list.push(listInputs);
+      setTodos(newTodos);
+      setListInputes("");
+    } work but there us better way*/
+  }
 
   return (
     <>
@@ -34,11 +59,28 @@ const handleDeleteTodo=(index)=>{
         </div>
       </div>
       <div className="todo_main">
-        {todos.map((item, index) => (
-          <div key={index} className='todo-card'>
+        {todos.map((todo) => (
+          <div key={todo.id} className='todo-card'>
             <div className='heading_todo'>
-              <h3>{item.heading}</h3>
-              <button className='delete-button-heading' onClick={() => handleDeleteTodo(index)}>delete Heading</button>
+              <h3>{todo.heading}</h3>
+              <button className='delete-button-heading' onClick={() => handleDeleteTodo(todo.id)}>delete Heading</button>
+            </div>
+            <div className='add_list'>
+              <ul>
+                {todo.list.map((item, index) => (
+                  <li key={index} className='todo_inside_list'>
+                    <p>{item}</p>
+                  </li>
+                ))}
+              </ul>
+              <input
+                type="text"
+                className='list-input'
+                placeholder='Add List'
+                value={listInputs[todo.id] || ""}
+                onChange={e => setListInputes(prev => ({ ...prev, [todo.id]: e.target.value }))}
+              />
+              <button className='add-list-button' onClick={() => handleAddList(todo.id)}>Add List</button>
             </div>
           </div>
         ))}
